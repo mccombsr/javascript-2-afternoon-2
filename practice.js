995edb6f27f989e52140a3169caf88d8de42f52a
Ryan McCombs <mccombs008@yahoo.com>
2018-09-11 15:43:23 -0600

    first submission

--- practice.js
+++ practice.js
@@ -18,6 +18,10 @@ var arr = [10,20,30];
 */
 
 //Code Here
+function first(arr) {
+  return arr[0];
+}
+
 
 
 
@@ -33,6 +37,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
+function last(arr) {
+  return arr[2];
+}
 
 
 
@@ -48,7 +55,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper(family) {
+  for (var i = 0; i < family.length; i++) {
+    alert(family[i]);       
+  }
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -63,6 +74,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+function reversedLooper(letters) {
+  for (let i = letters.length -1; i > -1; i--) {
+    alert(letters[i]);
+  }
+}
 
 
 
@@ -78,7 +94,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
-
+function evenFinder(nums) {
+  var evens = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) {
+      evens.push(nums[i]);
+    }    
+  }
+  return evens;
+}
 
 
 
